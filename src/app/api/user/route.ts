e278c9dc9c217d598a3c64f0b53d49e2af7e98ad
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-09-30 00:39:49 +0530

    added zod validation

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,23 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
+import { z } from "zod";
 
 export async function GET() {
   return NextResponse.json({ success: true });
 }
+//Defineing the schema for the input fields
+const userSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+});
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, username, password } = body;
+    const { email, username, password } = userSchema.parse(body);
 
     const existingUserByEmail = await db.user.findUnique({
       where: { email: email },
